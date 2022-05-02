7d8c829b66f0d9ba7b514f084d7eb06dcd2fad6b
Blazeleo <satviksams@gmail.com>
2022-05-02 12:15:55 +0530

    Pray

---
+++ pages/data-ig.js
@@ -0,0 +1,13 @@
+export var Faculty = [
+{id:"1",name:"A",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"10"},
+{id:"2",name:"B",Desig:"Associate Professor",Dept:"ISE",status:"notActive",lastServed:"2"},
+{id:"3",name:"C",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"5"},
+{id:"4",name:"D",Desig:"Professor",Dept:"ISE",status:"Active",lastServed:"3"},
+{id:"5",name:"E",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"10"}
+]
+
+var classroom = [
+{dep:"ISE",class_list:[301,302,303,304,305]},
+{dep:"CSE",class_list:[401,402,403,404]},
+{dep:"ECE",class_list:[501,502,503]}
+]
