ec1fd5b4678396e95cdc33dc808cf356ec4b26c8
Kalyan <kalyanb2000@gmail.com>
2022-05-02 10:42:04 +0530

    temp data

---
+++ data-ig.js
@@ -0,0 +1,13 @@
+var Faculty = [
+{id:"1",name:"A",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"10hrs ago"},
+{id:"2",name:"B",Desig:"Associate Professor",Dept:"ISE",status:"notActive",lastServed:"24hrs ago"},
+{id:"3",name:"C",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"5hrs ago"},
+{id:"4",name:"D",Desig:"Professor",Dept:"ISE",status:"Active",lastServed:"3hrs ago"},
+{id:"5",name:"E",Desig:"Assistant Professor",Dept:"ISE",status:"Active",lastServed:"10hrs ago"}
+]
+
+var classroom = [
+{dep:"ISE",class_list:[301,302,303,304,305]},
+{dep:"CSE",class_list:[401,402,403,404]},
+{dep:"ECE",class_list:[501,502,503]}
+]
