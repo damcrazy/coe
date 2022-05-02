fea72abf309267c43189245375c44d79a1bac1d3
AMITH P KASHYAP <57654909+amithkashyap2001@users.noreply.github.com>
2022-05-02 12:43:47 +0530

    Revert "test"
    This reverts commit e850faa8d8bcc3158b817c4d1aaa8efefc961ef6.

--- pages/parse.js
+++ pages/parse.js
@@ -1,18 +1,22 @@
-import { Faculty } from './data-ig'
+import { Faculty } from "./data-ig";
 
 console.log(Faculty)
 
-var dates = ['12/6/22', '12/7/22', '12/8/22', '12/9/22', '12/10/22', '12/11/22']
-var ordering = []
+var dates = ['12/6/22', '12/7/22', '12/8/22', '12/9/22', '12/10/22', '12/11/22'];
+var ordering = [];
 
-ordering = Faculty.sort(function (a, b) {
-  var x = parseInt(a['lastServed'], 10)
-  var y = parseInt(b['lastServed'], 10)
-  return x < y ? -1 : x > y ? 1 : 0
-})
+ordering = Faculty.sort(
+    function(a,b){
+        var x = parseInt(a['lastServed'],10); var y = parseInt(b['lastServed'],10);
+    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    }
+);
 
 export default function parse() {
-  return <div> helloooo </div>
-}
+    return (
+      <div> hi </div>
+    )
+  }
+  
 
-console.log(ordering)
+console.log(ordering);
